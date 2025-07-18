ecf18dd9931bbfc7c3143ea2cb6dc13d6cb23497
Tomáš Bosák <tombos255@gmail.com>
2025-07-18 22:58:08 +0200

    Working build [18.07.2025 22:58:08]

--- modules/bash.nix
+++ modules/bash.nix
@@ -1,14 +1,47 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      ll = "ls -lah";
-    };
-    initExtra = ''
-      # Custom bash initialization
-      export EDITOR=nvim
-    '';
-  };
+	programs.bash = {
+    	enable = true;
+    	shellAliases = {
+    		ll = "ls -lah";
+	  		reboot = "systemctl reboot";
+			shutdown = "systemctl poweroff";
+			uefi = "systemctl reboot --firmware-setup";
+			reload = "clear && source $BASHRC";
+			ducks = "sudo du -ahxd1 | sort -hr | head -n 11";
+			mv = "mv -i";
+			icat = "kitten icat";
+			chomd = "chmod";
+			gut = "git";
+			mdkir = "mkdir";
+			filezilla="nohup filezilla > /dev/null 2>&1 &";
+			librewolf="nohup librewolf -P > /dev/null 2>&1 &";
+		};
+		
+		initExtra = ''
+			alias cd..="cd ..";
+
+			# Source Bash Prompt
+			if [[ -f "$HOME/.bash_prompt" ]]; then
+				source "$HOME/.bash_prompt"
+			else
+				export PS1="\u@\h:\w\$ "
+			fi
+
+			# Load Custom exports and paths
+			if [[ -d "$HOME/.exports" ]]; then
+				for file in "$HOME/.exports/"*; do
+					if [[ -f "$file" ]]; then
+						source "$file"
+					fi
+				done
+			fi
+
+			# Source Bash functions
+			if [[ -f "$HOME/.bash_functions" ]]; then
+    			source "$HOME/.bash_functions"
+			fi
+		'';
+	};
 }
